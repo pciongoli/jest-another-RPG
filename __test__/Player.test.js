3f2179b8cc7fe4070271f4e0b820db5c5c65af18
pcion <patrick.ciongoli@gmail.com>
2022-03-03 02:58:13 -0500

    started the player object

---
+++ __test__/Player.test.js
@@ -0,0 +1,32 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const Player = require('../lib/Player');
+
+test('create a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+});
